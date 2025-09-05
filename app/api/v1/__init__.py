06a5f60f0d57354e750f8cea8f3a7a845f41d6e1
Dilagh <Dilagh01@gmail.com>
2025-09-05 05:34:47 +0000

    Auto-deploy: 2025-09-05 05:34:47

--- app/api/v1/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from .endpoints import auth, projects, items, calculations
-
-router = APIRouter()
-router.include_router(auth.router, prefix="/auth", tags=["auth"])
-router.include_router(projects.router, prefix="/projects", tags=["projects"])
-router.include_router(items.router, prefix="/items", tags=["items"])
-router.include_router(calculations.router, prefix="/calculations", tags=["calculations"])

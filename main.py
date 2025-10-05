59a07604ae07fa6452196dbf3394abf3bfb2534b
Dilagh <Dilagh01@gmail.com>
2025-10-05 04:22:14 +0000

    Auto-deploy: 2025-10-05 04:22:13

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from api.v1.endpoints.main import app
-import uvicorn
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'api')))
+
+from v1.endpoints.main import app
+import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # پورت پیش‌فرض Render
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)

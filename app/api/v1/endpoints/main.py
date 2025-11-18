4e2c57537028bc9590e81830db4667f726823b8b
Dilagh <Dilagh01@gmail.com>
2025-11-18 15:25:39 +0000

    Auto-deploy: 2025-11-18 15:25:38

--- app/api/v1/endpoints/main.py
+++ app/api/v1/endpoints/main.py
@@ -193,5 +193,5 @@ async def health():
 # ---------------------------------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)o
-
+    print(f"✅ سرور در پورت {port} اجرا شد")
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
